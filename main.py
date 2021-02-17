e49303e01a52e37fce9abf534e02ac5fd3e93aad
Alessandro Stockman <alessandrostockman@gmail.com>
2021-02-17 08:46:32 +0100

    minor fix

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ param_bag = ParameterList({
     "output_directory": "output",
     "threshold": [37],
     "distance": [distance_euclidean],
+    "morph_ops": [
+        MorphOpsSet(MorphOp(cv2.MORPH_OPEN, (3,3)), MorphOp(cv2.MORPH_CLOSE, (5,5), iterations=3), MorphOp(cv2.MORPH_CLOSE, (40,3)))#MorphOp(cv2.MORPH_CLOSE, (10,3), iterations=3))
+    ],
+    "background_threshold": [37],
+    "background_distance": [distance_euclidean],
     "background_morph_ops": [
         MorphOpsSet(MorphOp(cv2.MORPH_CLOSE, (3,3), iterations=1),MorphOp(cv2.MORPH_OPEN, (3,3), iterations=2), MorphOp(cv2.MORPH_DILATE, (25,10)), MorphOp(cv2.MORPH_ERODE, (15,5)))
     ],
     "alpha": [0.1],
-    "morph_ops": [
-        MorphOpsSet(MorphOp(cv2.MORPH_OPEN, (3,3)), MorphOp(cv2.MORPH_CLOSE, (5,5), iterations=3), MorphOp(cv2.MORPH_CLOSE, (40,3)))#MorphOp(cv2.MORPH_CLOSE, (10,3), iterations=3))
-    ],
     "background": {
         "frames": [100],
         "interpolation": [np.median]
