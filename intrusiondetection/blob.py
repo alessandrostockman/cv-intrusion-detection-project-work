fa70b48b1ed5463f895114a522ca6243079529cc
Alessandro Stockman <alessandrostockman@gmail.com>
2021-03-14 11:19:37 +0100

    add blob comments

--- intrusiondetection/blob.py
+++ intrusiondetection/blob.py
@@ -38,6 +38,9 @@ class Blob:
         self.compute_features()
 
     def compute_features(self):
+        '''
+            Wrapper method for the computation of all the blob features used
+        '''
         moments = cv2.moments(self.__main_contours)
 
         self.perimeter = round(cv2.arcLength(self.__main_contours, True), 2)
@@ -46,9 +49,15 @@ class Blob:
         self.cy = int(moments['m01'] / self.area)
 
     def attributes(self):
+        '''
+            List of attributes printed in the text output
+        '''
         return [self.id, self.area, self.perimeter, self.cx, self.cy, self.__cs, self.__es, self.__is_present, self.__blob_class]
 
     def parse_contours(self, image):
+        '''
+            Returns the contours of the blob found by OpenCV's findContours function
+        '''
         ret = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if len(ret) > 2:
             _, contours, hierarchy = ret
@@ -59,7 +68,7 @@ class Blob:
 
     def search_matching_blob(self, candidate_blobs, similarity_threshold):
         '''
-            Detcting matching blobs using the dissimilarity method shown below
+            Searches for a correspondence for the current blob in a list of candidates based on their similarity score
         '''
         match = None
         if len(candidate_blobs) > 0:
@@ -80,7 +89,7 @@ class Blob:
 
     def similarity_score(self, other):
         '''
-            Calculating the similarity of two blobs, as lower the dissimilarity as more likely the two blobs represent the same one in two different frames
+            Computation of two blobs' normalized similarity score
         '''
         area_diff = abs(other.area - self.area)
         area_diff_norm = area_diff / self.__frame_pixels
@@ -89,11 +98,18 @@ class Blob:
         return round((1 - ((area_diff_norm + barycenter_dist_norm) / 2)) * 100, 2)
 
     def classification_score(self):
+        '''
+            Lazy computation of the classification score performed by normalizing the blob area
+        '''
         if self.__cs == None:
             self.__cs = round(self.area / self.__frame_pixels * 100, 2)
         return self.__cs
 
     def edge_score(self, edge_adaptation=1):
+        '''
+            Lazy computation of the edge score performed by computing the Sobel operator over all the blob countours pixels and weights 
+            it with respect to the edge score of its match in the previous frame if present
+        '''
         #Calculating the derivatives to obtain the gradient value to verify if the object is a true object or a fake one
         if self.__es == None:
             val = 0
@@ -126,13 +142,18 @@ class Blob:
         return self.__es
 
     def classify(self, classification_threshold):
+        '''
+            Classifies the blob into PERSON or OBJECT using the classification score and sets the blob color for the final output
+        '''
         #Distinguish wether a blob is a person or an object in base of the are of his blob 
         self.__blob_class = BlobClass.PERSON if self.classification_score() > classification_threshold else BlobClass.OBJECT
         self.color = self.color_palette[self.color_map[self.__blob_class]]
         return self.__blob_class
 
     def detect(self, edge_threshold, edge_adaptation):
-        #detecting true blob from fake one in base of the gradient of the value of the edge
+        '''
+            Determines the presence of the blob by thresholding the edge score and sets the blob color for the final output
+        '''
         self.__is_present = self.edge_score(edge_adaptation=edge_adaptation) > edge_threshold
         if not self.__is_present:
             self.color = self.color_palette[self.color_map[BlobClass.FAKE]]
@@ -142,6 +163,7 @@ class Blob:
         font = cv2.FONT_HERSHEY_SIMPLEX
         textsize = cv2.getTextSize(text, font, scale, thickness)[0]
         offset = textsize[0] // 2
+        #TODO: Text centering
         #center = (self.cx - offset, self.cy - offset)
         center = (self.cx, self.cy)
         cv2.putText(image, text, center, font, scale, (255,255,255), thickness, cv2.LINE_AA)
